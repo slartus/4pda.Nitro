a68b8b71cc78bc3bacc2cd159c0c2568132401ed
Emigrate <krmisha45@gmail.com>
2014-01-13 10:52:45 +0200

    Запустился

--- Externals/4pdaApi/gen/ru/forpda/api/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package ru.forpda.api;
-
-/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
-public final class Manifest {
-}
---
+++ Externals/Http/gen/ru/forpda/Http/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ru.forpda.Http;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
